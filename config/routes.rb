adc3056443b4798edd40d8e6ccc58a201b7e7937
loubana alarabi <loubana@dconfig.com>
2021-07-08 05:46:08 +0300

    sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
+  get '/pages/about' => 'pages#about'
+  resources :signups
+  get '/pages/thanks' => 'pages#thanks'
+
+
 end
